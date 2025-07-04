112306e692ab0d7c1848ab36a2c93ae77e5ba1b8
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-04 15:23:42 +0200

    feat(KeyCounter): create key counter component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { CountClicks } from "./components/CountClicks/CountClicks";
 import { DoubleClickCounter } from "./components/DoubleClickCounter/DoubleClickCounter";
 import { HoverBox } from "./components/HoverBox/HoverBox";
+import { KeyCounter } from "./components/KeyCounter/KeyCountr";
 import { LoginForm } from "./components/LoginForm/LoginForm";
 import { MirrorInput } from "./components/MirrorInput/MirrorInput";
 import { ToggleText } from "./components/ToggleText/ToggleText";
@@ -9,12 +10,13 @@ import { ToggleText } from "./components/ToggleText/ToggleText";
 function App() {
 	return (
 		<>
-			<CountClicks /> 
-      <MirrorInput /> 
-      <LoginForm /> 
-      <ToggleText />
-      <DoubleClickCounter />
-      <HoverBox />
+			<CountClicks />
+			<MirrorInput />
+			<LoginForm />
+			<ToggleText />
+			<DoubleClickCounter />
+			<HoverBox />
+			<KeyCounter />
 		</>
 	);
 }
---
+++ src/components/KeyCounter/KeyCountr.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./KeyCounter.css";
+
+export const KeyCounter = () => {
+	const [text, setText] = useState("");
+	const [pressedKeys, setPressedKeys] = useState(0);
+
+	const handleText = (event) => {
+		setText(event.target.value);
+	};
+
+	const handlePressedKeys = () => {
+		if (text === "") {
+			setPressedKeys(0);
+		}
+		setPressedKeys((prev) => prev + 1);
+	};
+
+	return (
+		<div className="key-counter">
+			<label htmlFor="keys">Write here whatever you want and ill show you:</label>
+			<input type="text" id="keys" value={text} onChange={handleText} onKeyDown={handlePressedKeys} />
+
+			<h1>{text ? text + ", Pressed keys:" + pressedKeys : ""}</h1>
+		</div>
+	);
+};
