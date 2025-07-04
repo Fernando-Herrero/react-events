a2b9e33bbb929228b2fe88c6231095c0df717d89
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-04 13:43:20 +0200

    feat(ToggleText): add useEffect to toggle text component to show img and text for cr7

--- src/components/ToggleText/ToggleText.jsx
+++ src/components/ToggleText/ToggleText.jsx
@@ -1,13 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./ToggleText.css";
 import crImage from "../../assets/cr-siu.jpg";
 
 export const ToggleText = () => {
 	const [text, setText] = useState(true);
+	const [siuText, setSiuText] = useState("");
+	const crText = "SIIUUUU";
 
 	const handleText = () => {
-		setText(!text);
+		setText((prev) => !prev);
 	};
+
+	useEffect(() => {
+		let timeOutId;
+
+		const typeText = (index) => {
+			if (index < crText.length) {
+				setSiuText((prev) => prev + crText[index]);
+
+				timeOutId = setTimeout(() => {
+					typeText(index + 1);
+				}, 200);
+			}
+		};
+
+		if (text) {
+			setSiuText("");
+			typeText(0);
+		} else {
+			setSiuText("");
+		}
+
+		return () => clearTimeout(timeOutId);
+	}, [text]);
 
 	return (
 		<div className="toggle-text">
@@ -17,6 +42,7 @@ export const ToggleText = () => {
 					<h1>Cristiano Ronaldo is the GOAT</h1>
 					<div>
 						<img src={crImage} alt="Cristiano Ronaldo Image" />
+						<span>{siuText}</span>
 					</div>
 				</>
 			)}
